46c31c50549178aa82ebdb30447a377cb534b7ec
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-06 12:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,17 +5,12 @@ document.getElementById("btn").addEventListener("click", (e)=> {
   const name = document.getElementById("name").value;
   const age = document.getElementById("age").value;
 
-  if (!name || !age) {
-    alert("Please enter valid details.");
-    return;
-  }
-
   new Promise((resolve, reject) => {
     setTimeout(() => {
       if (age >= 18) {
         resolve(`Welcome, ${name}. You can vote.`);
       } else {
-        reject(`Oh sorry ${name}. You aren't old enough.`);
+        reject(`Oh sorry ${name}.You aren't old enough.`);
       }
     }, 4000);
   })
