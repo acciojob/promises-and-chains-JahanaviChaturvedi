4a1a6ecdd7ffa88708a8637ece4f025bbba5a78f
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-06 12:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.getElementById("btn").addEventListener("click", (e)=> {
-  event.preventDefault();
+  e.preventDefault();
 
   const name = document.getElementById("name").value;
   const age = document.getElementById("age").value;
